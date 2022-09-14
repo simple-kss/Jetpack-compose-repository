8d0f7da9d4b9b5ee047ef3c5bbb713eba2f4319c
Seongsik Kim <kss7858@naver.com>
2022-09-15 01:08:38 +0900

    update 2022/09/15 - 02
    Signed-off-by: Seongsik Kim <kss7858@naver.com>

--- simple-login-ui-master/app/src/main/java/com/learnandroid/loginapplication/composables/JobSearch/ListItem.kt
+++ simple-login-ui-master/app/src/main/java/com/learnandroid/loginapplication/composables/JobSearch/ListItem.kt
@@ -115,7 +115,9 @@ fun JobItem(data : JobInfoData){
                 Column(modifier = Modifier.padding(
                     bottom = extraPadding.coerceAtLeast(0.dp)
                 )) {
-                    Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                    val str: String = data.toString()
+                    Text(text = str)
+                    // "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
                 }
             }
         }
